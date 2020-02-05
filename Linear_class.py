aed5f8d35099d7eceefdddee6f4ed75743944d8a
BrahimEG <>
2020-02-05 14:38:34 +0100

    Addes first player skill feature (FG%)

--- Linear_class.py
+++ Linear_class.py
@@ -4,7 +4,167 @@ Created on Tue Jan 28 16:13:32 2020
 
 @author: Ibrahim
 """
+import pandas as pd 
+import numpy as np
+from basketball_reference_web_scraper import client
+from basketball_reference_web_scraper.data import OutputType
+import os
+import glob
+import datetime
+from datetime import timedelta
+import sklearn as sk
+from sklearn.linear_model import LinearRegression as LR
+from sklearn.linear_model import LogisticRegression as LogR
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
+from sklearn.neural_network import MLPClassifier
+from preprocess_data import unique_players
+
+#import data from 
+merged_data = pd.read_json('./data/merged_data.json')
+
+merged_data = merged_data.sample(frac=1).reset_index(drop=True)
+
+#Create train, validation and test-sets
+train_end = round(0.6*len(merged_data))
+valid_end = train_end + (round(0.2*len(merged_data)))
+train_index = range(0, train_end)
+valid_index = range(train_end, valid_end)
+test_index = range(valid_end, len(merged_data))
+ 
+ 
+train_x = np.array(list(merged_data['player_array'][0:train_end]))
+train_y = np.array(list(merged_data['win'][0:train_end])).reshape(train_end,1)
+ 
+valid_x = np.array(list(merged_data['player_array'][train_end:valid_end]))
+valid_y = np.array(list(merged_data['win'][train_end:valid_end])).reshape((valid_end - train_end),1)
+
+test_x = np.array(list(merged_data['player_array'][valid_end:len(merged_data)]))
+test_y = np.array(list(merged_data['win'][valid_end:len(merged_data)])).reshape((len(merged_data) - valid_end),1)
+
+# Create Logistic regression object
+regr = LogR()
+
+# Train the model using the training sets
+regr.fit(train_x, train_y)
+
+# Make predictions using the training set and test-set
+train_pred = regr.predict(train_x)
+test_pred = regr.predict(test_x)
+
+print('train_accuracy_score:', accuracy_score(train_y, train_pred))    
+print('test_accuracy_score:', accuracy_score(test_y, test_pred)) 
 
 
 
-os.getcwd()
+#create MLP object and train with complete data set
+class_1 = MLPClassifier(solver='adam', hidden_layer_sizes = (40,) , activation = 'logistic')
+class_1.fit(train_x, train_y)
+
+class_1.score(train_x, train_y)
+class_1.score(test_x, test_y)
+
+
+#Instead of binary classification of winning use regression of point difference to estimate winner
+
+merged_data['point_diff'] = merged_data['home_team_score'] - merged_data['away_team_score']
+
+train1_x = np.array(list(merged_data['player_array'][0:train_end]))
+train1_y = np.array(list(merged_data['point_diff'][0:train_end])).reshape(train_end,1)
+
+test1_x = np.array(list(merged_data['player_array'][valid_end:len(merged_data)]))
+test1_y = np.array(list(merged_data['point_diff'][valid_end:len(merged_data)])).reshape((len(merged_data) - valid_end),1)
+
+
+#create LR and MLP object and fit on training set ussing point-diff as y-labels instead of binaray-win-labels
+regr1 = LR()
+regr1.fit(train1_x, train1_y)
+
+
+train1_pred = regr1.predict(train1_x)
+train1_pred = train1_pred > 0
+train1_pred = train1_pred.astype(int)
+
+test1_pred = regr1.predict(test1_x)
+test1_pred = test1_pred > 0
+test1_pred = test1_pred.astype(int)
+
+
+print('train_accuracy_score:', accuracy_score(train_y, train1_pred))
+print('test_accuracy_score:', accuracy_score(test_y, test1_pred)) 
+
+
+class_2 = MLPClassifier(solver='adam', hidden_layer_sizes = (40,) , activation = 'logistic')
+class_2.fit(train1_x, train1_y)
+
+a = class_2.predict(train1_x)
+a = a > 0
+a = a.astype(int)
+
+b = class_2.predict(test1_x)
+b = b > 0
+b = b.astype(int)
+
+print('train_accuracy_score:', accuracy_score(train_y, a))
+print('test_accuracy_score:', accuracy_score(test_y, b))
+
+
+#Use two estimators (player_array and field-goal percentage) to predict win-probability
+
+uniq_players = unique_players()
+
+merged_data['field_goal_array_home'] = None
+merged_data['field_goal_array_away'] = None
+merged_data['field_goal_array'] = None
+    
+for i in range(len(merged_data)):
+    print(i)
+    temp_home = {}
+    temp_away = {}
+    for x in uniq_players:
+        temp_home[x] = 0
+        temp_away[x] = 0
+        for y in merged_data['players'][i].keys():
+            if y == x:
+                if merged_data['players'][i][y]['team'] == merged_data['home_team'][i]:
+                    try:
+                        temp_home[x] = merged_data['players'][i][y]['made_field_goals'] / merged_data['players'][i][y]['attempted_field_goals']
+                    except:
+                        temp_home[x] = 0
+                elif merged_data['players'][i][y]['team'] == merged_data['away_team'][i]:
+                    try:
+                        temp_away[x] = merged_data['players'][i][y]['made_field_goals'] / merged_data['players'][i][y]['attempted_field_goals']
+                    except:
+                        temp_away[x] = 0
+    merged_data['field_goal_array_home'][i] = np.array(list(temp_home.values()))
+    merged_data['field_goal_array_away'][i] = np.array(list(temp_away.values()))
+    merged_data['field_goal_array'][i] = np.concatenate((merged_data['field_goal_array_home'][i], merged_data['field_goal_array_away'][i]))
+
+
+
+train_x = np.array(list(merged_data['player_array'][0:train_end]))
+train_field_x = np.array(list(merged_data['field_goal_array'][0:train_end]))
+train_y = np.array(list(merged_data['win'][0:train_end])).reshape(train_end,1)
+ 
+valid_x = np.array(list(merged_data['player_array'][train_end:valid_end]))
+valid_field_x = np.array(list(merged_data['field_goal_array'][train_end:valid_end]))
+valid_y = np.array(list(merged_data['win'][train_end:valid_end])).reshape((valid_end - train_end),1)
+
+test_x = np.array(list(merged_data['player_array'][valid_end:len(merged_data)]))
+test_field_x = np.array(list(merged_data['field_goal_array'][valid_end:len(merged_data)]))
+test_y = np.array(list(merged_data['win'][valid_end:len(merged_data)])).reshape((len(merged_data) - valid_end),1)
+
+
+
+regr2 = LogR()
+regr2.fit(np.concatenate((train_x,train_field_x), axis = 1), train_y)
+
+train_field_pred = regr2.predict(np.concatenate((train_x,train_field_x), axis = 1))
+test_field_pred = regr2.predict(np.concatenate((test_x,test_field_x), axis = 1))
+
+print('train_accuracy_score:', accuracy_score(train_y, train_field_pred))    
+print('test_accuracy_score:', accuracy_score(test_y, test_field_pred)) 
+
+
+
+
+    
--- preprocess_data.py
+++ preprocess_data.py
@@ -15,6 +15,8 @@ import sklearn as sk
 from sklearn.linear_model import LinearRegression as LR
 from sklearn.linear_model import LogisticRegression as LogR
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
+from sklearn.neural_network import MLPClassifier
+
 
 #gather all game results sorted by date
 def upload_data():
@@ -23,6 +25,7 @@ def upload_data():
     client.season_schedule(season_end_year=2017, output_type=OutputType.CSV,  output_file_path = './game_results_2016_2017.csv')
     client.season_schedule(season_end_year=2018, output_type=OutputType.CSV,  output_file_path = './game_results_2017_2018.csv')
     
+    client.players_season_totals(season_end_year=2014, output_type=OutputType.JSON,  output_file_path = './player_stats_2013_2014.json')
     client.players_season_totals(season_end_year=2015, output_type=OutputType.JSON,  output_file_path = './player_stats_2014_2015.json')
     client.players_season_totals(season_end_year=2016, output_type=OutputType.JSON,  output_file_path = './player_stats_2015_2016.json')    
     client.players_season_totals(season_end_year=2017, output_type=OutputType.JSON,  output_file_path = './player_stats_2016_2017.json')
@@ -49,7 +52,7 @@ def process_gamedata():
     player_stats = process_playerdata()
     
     counter = 0
-    for file in glob.glob('*.csv'):
+    for file in glob.glob('./data/*.csv'):
         temp = pd.read_csv(file)
         temp['players'] = None
         for i in range(len(temp)):
@@ -61,19 +64,31 @@ def process_gamedata():
             if (counter == 0):
                 for x in player_stats['2015'].keys():
                     if temp['home_team'][i] == player_stats['2015'][x]['team'] or temp['away_team'][i] == player_stats['2015'][x]['team']:
-                        temp['players'][i][x] = player_stats['2015'][x]
+                        try:
+                            temp['players'][i][x] = player_stats['2014'][x]
+                        except KeyError:
+                            temp['players'][i][x] = pd.Series({'team': player_stats['2015'][x]['team']})                
             elif (counter == 1):
                 for x in player_stats['2016'].keys():
                     if temp['home_team'][i] == player_stats['2016'][x]['team'] or temp['away_team'][i] == player_stats['2016'][x]['team']:
-                        temp['players'][i][x] = player_stats['2016'][x]
+                        try:
+                            temp['players'][i][x] = player_stats['2015'][x]
+                        except KeyError:
+                            temp['players'][i][x] = pd.Series({'team' : player_stats['2016'][x]['team']})
             elif (counter == 2):
                 for x in player_stats['2017'].keys():
                     if temp['home_team'][i] == player_stats['2017'][x]['team'] or temp['away_team'][i] == player_stats['2017'][x]['team']:
-                        temp['players'][i][x] = player_stats['2017'][x]
+                        try:
+                            temp['players'][i][x] = player_stats['2016'][x]
+                        except KeyError:
+                            temp['players'][i][x] = pd.Series({'team' : player_stats['2017'][x]['team']})
             elif (counter == 3):
                 for x in player_stats['2018'].keys():
                     if temp['home_team'][i] == player_stats['2018'][x]['team'] or temp['away_team'][i] == player_stats['2018'][x]['team']:
-                        temp['players'][i][x] = player_stats['2018'][x]
+                        try:
+                            temp['players'][i][x] = player_stats['2017'][x]
+                        except KeyError:
+                            temp['players'][i][x] = pd.Series({'team': player_stats['2018'][x]['team']})
         counter += 1
         all_data = pd.concat([all_data, temp])
     
@@ -86,17 +101,21 @@ def process_gamedata():
 
 #gather all seasonal player-stats and store them by player
 def process_playerdata():
-    stats_2015 = pd.read_json('player_stats_2014_2015.json')
-    stats_2016 = pd.read_json('player_stats_2015_2016.json')
-    stats_2017 = pd.read_json('player_stats_2016_2017.json')
-    stats_2018 = pd.read_json('player_stats_2017_2018.json')
+    stats_2014 = pd.read_json('./data/player_stats_2013_2014.json')
+    stats_2015 = pd.read_json('./data/player_stats_2014_2015.json')
+    stats_2016 = pd.read_json('./data/player_stats_2015_2016.json')
+    stats_2017 = pd.read_json('./data/player_stats_2016_2017.json')
+    stats_2018 = pd.read_json('./data/player_stats_2017_2018.json')
     
     player_stats = {}
+    player_stats['2014'] = {}
     player_stats['2015'] = {}
     player_stats['2016'] = {}
     player_stats['2017'] = {}
     player_stats['2018'] = {}
     
+    for x in range(len(stats_2014)):
+        player_stats['2014'][stats_2014['name'][x]] = stats_2014.loc[x]
     for x in range(len(stats_2015)):
         player_stats['2015'][stats_2015['name'][x]] = stats_2015.loc[x]
     for x in range(len(stats_2016)):
@@ -105,66 +124,55 @@ def process_playerdata():
         player_stats['2017'][stats_2017['name'][x]] = stats_2017.loc[x]
     for x in range(len(stats_2015)):
         player_stats['2018'][stats_2018['name'][x]] = stats_2018.loc[x]
-    
-    
-    
+   
     return player_stats
 
 
 def unique_players():
-    merged_data = pd.read_json(r'./merged_data.json')
-    
+    player_data = process_playerdata()
     uniq_players = {None}
     set(uniq_players)
-    for i in range(len(merged_data)):
-        for x in merged_data['players'][i].keys():
+    years = ['2015', '2016', '2017', '2018']
+    for i in years:
+        for x in player_data[i].keys():
             uniq_players.add(x)
     
     return uniq_players
 
-
-
-def test():
-    merged_data = pd.read_json(r'./merged_data.json')
-    uniq_players = unique_players()
-    return merged_data, uniq_players
-
-
-if __name__ == "__main__":
-    merged_data, uniq_players = test()
+def create_playerarray():
+    merged_data, uniq_players = process_gamedata(), unique_players()
+    
+    #creating identity arrays for teams for every game
+    merged_data['player_array_home'] = None
+    merged_data['player_array_away'] = None
     merged_data['player_array'] = None
     merged_data['win'] = None
+    
     for i in range(len(merged_data)):
-        print(i)
+        print("2nd", i)
         if merged_data['home_team_score'][i] > merged_data['away_team_score'][i]:
             merged_data['win'][i] = 1
         else:
             merged_data['win'][i] = 0
-        temp = {}
+        temp_home = {}
+        temp_away = {}
         for x in uniq_players:
-            temp[x] = 0
+            temp_home[x] = 0
+            temp_away[x] = 0
             for y in merged_data['players'][i].keys():
                 if y == x:
-                    temp[x] = 1
-        merged_data['player_array'][i] = np.array(list(temp.values()))
-    
-merged_data = merged_data.sample(frac=1).reset_index(drop=True)
-
-   
-train_x = np.array(list(merged_data['player_array']))
-train_y = np.array(list(merged_data['win'])).reshape(5248,1)
- 
-# Create Logistic regression object
-regr = LogR()
-
-# Train the model using the training sets
-regr.fit(train_x, train_y)
-
-# Make predictions using the testing set
-y_pred = regr.predict(train_x)
+                    if merged_data['players'][i][y]['team'] == merged_data['home_team'][i]:
+                        temp_home[x] = 1
+                    elif merged_data['players'][i][y]['team'] == merged_data['away_team'][i]:
+                        temp_away[x] = 1 
+        merged_data['player_array_home'][i] = np.array(list(temp_home.values()))
+        merged_data['player_array_away'][i] = np.array(list(temp_away.values()))
+        merged_data['player_array'][i] = np.concatenate((merged_data['player_array_home'][i], merged_data['player_array_away'][i]))
+    return merged_data
 
 
-print('accuracy_score:', accuracy_score(train_y, y_pred))    
 
-    
+if __name__ == "__main__":
+    create_playerarray().to_json('./data/merged_data.json')
+    print('test')
     
